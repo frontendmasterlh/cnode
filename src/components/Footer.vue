920799f29ce268198e440ef4a22a48eb42f5726f
frontendmasterlh <2757144334@qq.com>
2019-10-21 08:41:25 +0800

    complish

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <div class="footer_main">
+      ©  gaohong  wechat:
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Footer'
+}
+</script>
+
+<style scoped>
+.footer{
+  text-align: center;
+  background: #fff;
+  width: 100%;
+}
+.footer_main{
+  color: #e2e2e2;
+  padding: 22px 0;
+  font-size: 13px;
+  line-height: 2em;
+}
+</style>
